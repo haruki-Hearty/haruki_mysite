6540349237240806318aabdc47ce0cb203da209b
haruki-Hearty <haruki1015go@gmail.com>
2021-03-07 11:20:32 +0900

    jsの整理とバーガーメニューの修正

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,27 @@
 "use strict";
 
+$(function () {
+  $('.header__buger-link').on('click', function (event) {
+    $('#header__buger-checkBox').prop('checked', false);
+  });
+}); // リンクをクリックしたらバガーメニューを閉じる
+
+$(function () {
+  $('.bgimg-slider').bgSwitcher({
+    images: ['dist/img/fv-bgi_01@2x.jpg', 'dist/img/fv-bgi_02@2x.jpg', 'dist/img/fv-bgi_03@2x.jpg'],
+    effect: "drop",
+    easing: "linear"
+  });
+}); // bgスライダー
+
+$(window).on('scroll', function () {
+  if ($('.MV').height() < $(this).scrollTop()) {
+    $('.header').addClass('change-color');
+  } else {
+    $('.header').removeClass('change-color');
+  }
+}); // ヘッダースクロールしたら色が変わる
+
 $('a[href^="#"]').click(function () {
   // スクロールの速度
   var speed = 400; // ミリ秒で記述
@@ -11,4 +33,4 @@ $('a[href^="#"]').click(function () {
     scrollTop: position
   }, speed, 'swing');
   return false;
-});
+}); // スムーススクロール
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,27 @@
+$(function () {
+    $('.header__buger-link').on('click',function(event) {
+        $('#header__buger-checkBox').prop('checked',false);
+    });
+});
+// リンクをクリックしたらバガーメニューを閉じる
+
+$(function() {
+  $('.bgimg-slider').bgSwitcher({
+      images: ['dist/img/fv-bgi_01@2x.jpg','dist/img/fv-bgi_02@2x.jpg','dist/img/fv-bgi_03@2x.jpg'],
+      effect: "drop",
+      easing: "linear"
+  });
+});
+// bgスライダー
+
+$(window).on('scroll', function () {
+  if ($('.MV').height() < $(this).scrollTop()) {
+  $('.header').addClass('change-color'); }
+  else {
+  $('.header').removeClass('change-color'); }
+});
+// ヘッダースクロールしたら色が変わる
+
 $('a[href^="#"]').click(function() {
   // スクロールの速度
   let speed = 400; // ミリ秒で記述
@@ -9,3 +33,4 @@ $('a[href^="#"]').click(function() {
   }, speed, 'swing');
   return false;
 });
+// スムーススクロール
